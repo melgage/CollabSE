f0871517e0f6d9615d636a86dca8d94094ae800f
melgage <mel.gage@smartbear.com>
2022-05-05 13:51:20 +0000

    Change Two REv 1

--- Main.java
+++ Main.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 // Comment One Rev 1
 //
 // Comment Two
-//
 
 public class Main {
 
@@ -17,7 +16,7 @@ public class Main {
             return 1;
         }
 
-        return fibonacciRecursion(number-1) + fibonacciRecursion(number -2); //tail recursion
+        return fibonacciRecursion(number-1) + fibonacciRecursion(number -2);
     }
     //
     //
@@ -26,7 +25,9 @@ public class Main {
         if(number == 1 || number == 2){
             return 1;
         }
-        long fibo1=1,fibo2=1,fibonacci=1;
+        long fibo1=1,
+		     fibo2=1,
+			 fibonacci=1;
         for(int i= 3; i<= number; i++){
             fibonacci = fibo1 + fibo2; //Fibonacci number is sum of previous two Fibonacci number
             fibo1 = fibo2;
