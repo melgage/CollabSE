e3ff3195db17f04cf30e9ecbb8f417116317701a
melgage <mel.gage@smartbear.com>
2022-05-24 17:51:08 +0000

    Change One Rev 1

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 // Initial Commit
 //
-// Change One
+// Change One Rev 1
 //
 //
 //
@@ -16,7 +16,7 @@ public class Main {
             return 1;
         }
 
-        return fibonacciRecusion(number-1) + fibonacciRecusion(number -2); //Tail recursion
+        return fibonacciRecursion(number-1) + fibonacciRecursion(number -2); //Tail recursion
     }
     //
     //
