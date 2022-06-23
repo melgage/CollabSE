18383cad5a99898c5f47a4edd34fdc16b0155e43
melgage <mel.gage@smartbear.com>
2022-06-23 21:27:20 +0000

    Change Two

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 //
 //
 //
-//
+// Change Two
 //
 
 public class Main {
@@ -16,7 +16,7 @@ public class Main {
             return 1;
         }
 
-        return fibonacciRecusion(number-1) + fibonacciRecusion(number -2); //Tail recursion
+        return fibonacciRecursion(number-1) + fibonacciRecursion(number -2); //Tail recursion
     }
     //
     //
