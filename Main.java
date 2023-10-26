d794ba3944f09ec010d50489470cca973e5a4f05
Mel Gage <mel.gage@smartbear.com>
2023-10-26 15:55:53 +0000

    Change Three Plus

--- Main.java
+++ Main.java
@@ -15,7 +15,7 @@ public class Main {
             return 1;
         }
 
-        return fibonacciRecusion(number-1) + fibonacciRecursion(number-2); //Tail recursion
+        return fibonacciRecursion(number-1) + fibonacciRecursion(number-2); //Tail recursion
     }
 	//
 	//
