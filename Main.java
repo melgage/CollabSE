7627e2f07a593cc8338bdf2e23a2b4cef5f31a15
Mel Gage <mel.gage@smartbear.com>
2026-01-16 18:22:55 +0000

    Update

--- Main.java
+++ Main.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 
 // Initial Commit
 //
-// Change One
-// Change Two
 //
 //
 public class Main {
@@ -16,7 +14,7 @@ public class Main {
             return 1;
         }
 
-        return fibonacciRecursion(number-1) + fibonacciRecursion(number-2); //Tail recursion
+        return fibonacciRecusion(number-1) + fibonacciRecursion(number-2); //Tail recursion
     }
 	//
 	//
